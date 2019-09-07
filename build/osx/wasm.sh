3685b6e972a6a89126aeca3dc757874eef06634c
FiveTech Software <antonio.fivetech@gmail.com>
2019-09-07 09:16:04 +0200

    Add files via upload

---
+++ build/osx/wasm.sh
@@ -0,0 +1,8 @@
+sh ../emsdk/emsdk_env.sh
+export PATH=$PATH:../emsdk/fastcomp/emscripten
+export LLVM_ROOT=/Users/$USER/emsdk/fastcomp/bin
+export HB_COMPILER=wasm
+export HB_PLATFORM=abstr
+export HB_HOST_BIN=/Users/$USER/viktor/bin/darwin/clang
+export HB_USER_CFLAGS=-DHB_NO_GZLIB
+make
